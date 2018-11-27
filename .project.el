0e397a64af567c8c9750e4a0a7d7700c0be0cebd
Francis Murillo <francisavmurillo@gmail.com>
2018-11-27 21:40:02 +0800

    Added arrows!!

--- .project.el
+++ .project.el
@@ -1,6 +1,4 @@
-(progn
-  (require 'magin)
-
+(when (require 'magin)
   (magin--dsl-defblock
    `(defblock org-jekyll-blogger-site
       (comment "Block for org-jekyll-blogger-site")
@@ -14,3 +12,22 @@
    `(delimited
      (block org-jekyll-blogger-site))
    (projectile-project-root)))
+
+(when (require 'prodigy)
+  (prodigy-define-tag
+    :name 'jekyll
+    :command "jekyll"
+    :args `("serve" "--host=0.0.0.0" "--port=34000")
+    :stop-signal 'kill)
+
+  (fn/prodigy-define-service
+   :name "fnlog-server"
+   :tags `(fnlog jekyll)
+   :port 34000
+    :args `("serve" "--host=0.0.0.0" "--port=34000")
+
+   :bind-command-name "fnlog"
+   :bind-map fn/prodigy-map
+   :bind (kbd "f f"))
+
+  (prodigy-start-service (prodigy-find-service "fnlog-server")))
