31134c39cd93617c6a56358db72e975f1cdedef7
Francis Murillo <francisavmurillo@gmail.com>
2019-03-09 22:01:25 +0800

    Ribbon kinda finalized

--- .project.el
+++ .project.el
@@ -1,30 +1,17 @@
-(when (require 'magin)
-  (magin--dsl-defblock
-   `(defblock org-jekyll-blogger-site
-      (comment "Block for org-jekyll-blogger-site")
-      (dir ".sass-cache")
-
-      (dir "_drafts")
-      (dir "_site"))
-   (list))
-
-  (magin-write-to-project
-   `(delimited
-     (block org-jekyll-blogger-site))
-   (projectile-project-root)))
+(defconst fbb/project-dir (projectile-project-root))
 
 (when (require 'prodigy)
   (prodigy-define-tag
     :name 'jekyll
     :command "jekyll"
-    :args `("serve" "--host=0.0.0.0" "--port=34000")
     :stop-signal 'kill)
 
   (fn/prodigy-define-service
    :name "fnlog-server"
    :tags `(fnlog jekyll)
    :port 34000
-    :args `("serve" "--host=0.0.0.0" "--port=34000")
+   :args `("serve" "--host=0.0.0.0" "--port=34000")
+   :cwd fbb/project-dir
 
    :bind-command-name "fnlog"
    :bind-map fn/prodigy-map
